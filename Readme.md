82e50c02019d99e829f085471a6eed71a52c0772
jesravnbol <jra@kapacity.dk>
2021-02-20 12:33:23 +0100

    QuoteEngine - wip.

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Meme generator
+
+Generate memes
+
+# Get started
+
+## Software dependencies
+
+- Python 3.7
+
+## Install
+
+```bash
+pip install -r requirements.txt
+```
+
+
+
---
+++ src/QuoteEngine/CSVIngestor.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from pandas import read_csv
+
+from src.QuoteEngine.IngestorInterface import IngestorInterface
+from src.QuoteEngine.QuoteModel import QuoteModel
+
+
+class CSVIngestor(IngestorInterface):
+    allowed_extensions = ['csv']
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        if not cls.can_ingest(path):
+            raise Exception('cannot ingest exception')
+
+        quotes = []
+        df = read_csv(path, header=0)
+
+        for index, row in df.iterrows():
+            quotes.append(
+                QuoteModel(author=row['author'], body=row['body'])
+            )
+        return quotes
+
+
